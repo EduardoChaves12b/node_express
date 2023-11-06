809cff909a3fcf8f7ed61930edde3ab3b69d9c6f
EA - Eduardo Chaves Custodio <eduardo_chaves@educadventista.org>
2023-11-06 15:36:37 -0300

    commit 2

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const path = require("path")
 
 const app = express()
 
-const caminho = path.join(__dirname, "templates")
+const caminhoBase = path.join(__dirname, "templates")
 
-app.get('/',(requisicao, resposta) => {
-    resposta.sendFile(`${caminhoBase}/index.html`)
+// https://localhost:3000/usuarios
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
 
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhobase}/usuarios.html`)
 })
- 
-app.listen (3000, () => {
-    console.log("servidor rodando na porta 3000")
+
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/index.html`)
+})
+
+app.listen(3000,() => {
+    console.log("Servidor rodando na porta 3000!")
 })
